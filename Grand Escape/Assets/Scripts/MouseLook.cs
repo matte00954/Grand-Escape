aabe80bed0ffa8ee5a1eda2255d3f827378fca7d
Eaglebranch <williamornquist@gmail.com>
2021-04-07 13:26:18 +0200

    Script Comment Update
    Added descriptive comments to two scripts:
    *PlayerMovement
    *MouseLook

--- Grand Escape/Assets/Scripts/MouseLook.cs
+++ Grand Escape/Assets/Scripts/MouseLook.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    
+    public float mouseSensitivity = 100f; //Default mouse sensitivity value. Can be changed otherwise in Inspector.
 
-    public float mouseSensitivity = 100f;
+    public Transform playerBody; //The player's model object.
 
-    public Transform playerBody;
-
-    float xRotation = 0f;
+    float xRotation = 0f; 
 
     // Start is called before the first frame update
     void Start()
     {
+        //This locks the mouse cursor to the game screen and hides it.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -23,10 +24,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
+        xRotation -= mouseY; //Looking up or down means you're rotating your view along the X-axis.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //This makes it so that you can't look further past than straight down and up.
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+
+        playerBody.Rotate(Vector3.up * mouseX); //This rotates the player model along the Y-axis when moving the mouse left or right.
     }
 }

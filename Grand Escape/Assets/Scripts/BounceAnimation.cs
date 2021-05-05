f980c3509b92af53b99e012e581197c7a0094100
mirandart <60674005+mirandart@users.noreply.github.com>
2021-05-05 18:54:35 +0200

    pause menu graphics
    grafik + ofärdigt script till en pausmeny
    
    aktiveras/inaktiveras när man trycker på escape, lyckas dock inte få den att fungera i level 1 eller level 2 då player shoot överskuggar knapptryck + är osäker hur jag ska göra med timescale

---
+++ Grand Escape/Assets/Scripts/BounceAnimation.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Unity.Mathfx;
+
+public class BounceAnimation : MonoBehaviour
+{
+    public float startTime = 0f;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = startTime;
+    }
+
+    void OnEnable()
+    {
+        time = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (time <= 1f)
+        {
+            transform.localScale = Vector3.one * Mathfx.Berp(0f, 1f, time);
+            time = time + Time.deltaTime * 1.5f;
+        }
+    }
+}
+
+public sealed class Mathfx : MonoBehaviour
+{
+    public static float Berp(float start, float end, float value)
+    {
+        value = Mathf.Clamp01(value);
+        value = (Mathf.Sin(value * Mathf.PI * (0.2f + 2.5f * value * value * value)) * Mathf.Pow(1f - value, 2.2f) + value) * (1f + (1.2f * (1f - value)));
+        return start + (end - start) * value;
+    }
+
+    public static Vector2 Berp(Vector2 start, Vector2 end, float value)
+    {
+        return new Vector2(Berp(start.x, end.x, value), Berp(start.y, end.y, value));
+    }
+
+    public static Vector3 Berp(Vector3 start, Vector3 end, float value)
+    {
+        return new Vector3(Berp(start.x, end.x, value), Berp(start.y, end.y, value), Berp(start.z, end.z, value));
+    }
+    
+}

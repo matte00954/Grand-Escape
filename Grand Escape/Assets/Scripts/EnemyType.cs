a7c79442615fa7185430e48124a13a80f68eaaef
matte00954 <matte00954@gmail.com>
2021-05-25 12:03:43 +0200

    Updated enemy type

--- Grand Escape/Assets/Scripts/EnemyType.cs
+++ Grand Escape/Assets/Scripts/EnemyType.cs
@@ -8,5 +8,13 @@ public class EnemyType : ScriptableObject
 
     [SerializeField] private int maxHealthPoints = 100;
 
+    [SerializeField] private int damage = 100;
+
+    //public int MaxHealthPoints { get => maxHealthPoints; set => maxHealthPoints = value; }
+
+    //public int Damage { get => damage; set => damage = value; }
+
     public int GetMaxHealthPoints() { return maxHealthPoints; }
+
+    public int GetDamage() { return damage; }
 }

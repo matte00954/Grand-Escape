913018ffe011e9d716e85c91cb6af54652dde2ff
TheWullie <54275945+TheWullie@users.noreply.github.com>
2021-05-25 18:08:25 +0100

    Smoke particles and sound system for boat cannons
    + New script BoatCannons on the boats with cannons (currently 2 in the scene)
    + New CannonSmoke particle effect (same as MuzzleSmoke but adjusted size)
    + Boats with cannons now make shooting sounds and instantiate a smoke particle effect at random intervals (changeable in the Inspector)

---
+++ Grand Escape/Assets/Scripts/BoatCannons.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatCannons : MonoBehaviour
+{
+    [SerializeField] private AudioClip cannonShot;
+    /*[SerializeField] private AudioClip shot2;
+    [SerializeField] private AudioClip shot3;
+    [SerializeField] private AudioClip shot4;*/
+    [SerializeField] private GameObject smokeHolder;
+    [SerializeField] private ParticleSystem cannonSmoke;
+    [SerializeField] private float minDelay;
+    [SerializeField] private float maxDelay;
+
+    private AudioSource source;
+    private bool soundIsPlaying;
+    private float randomTimer;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        /*sources[0].clip = shot1;
+        sources[1].clip = shot2;
+        sources[2].clip = shot3;
+        sources[3].clip = shot4;*/
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            soundIsPlaying = false;
+
+            randomTimer = Random.Range(minDelay, maxDelay);
+
+            source.PlayDelayed(randomTimer);
+        }
+
+        if (source.isPlaying & !soundIsPlaying)
+        {
+            Invoke("PlayParticleSystem", randomTimer);
+            soundIsPlaying = true;
+        }
+
+        /*if (!sources[1].isPlaying)
+        {
+            float d = Random.Range(minDelay, maxDelay);
+            sources[1].PlayDelayed(d);
+        }
+
+        if (!sources[2].isPlaying)
+        {
+            float d = Random.Range(minDelay, maxDelay);
+            sources[2].PlayDelayed(d);
+        }
+
+        if (!sources[3].isPlaying)
+        {
+            float d = Random.Range(minDelay, maxDelay);
+            sources[3].PlayDelayed(d);
+        }*/
+    }
+
+    void PlayParticleSystem()
+    {
+        Instantiate(cannonSmoke, smokeHolder.transform.position, smokeHolder.transform.rotation);
+    }
+}

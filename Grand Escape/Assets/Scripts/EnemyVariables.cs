7574ea59e12becfb6942414bc09913c11df36096
TheWullie <54275945+TheWullie@users.noreply.github.com>
2021-06-03 10:08:07 +0100

    Enemy layer weight
    • Removed if-statement so the melee enemy now has a working death animation

--- Grand Escape/Assets/Scripts/EnemyVariables.cs
+++ Grand Escape/Assets/Scripts/EnemyVariables.cs
@@ -27,10 +27,7 @@ public class EnemyVariables : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         startPosition = transform.position;
         anim = GetComponent<Animator>();
-        if (GetComponent<EnemyShooting>()!= null)
-        {
-            anim.SetLayerWeight(1, 1);
-        }
+        anim.SetLayerWeight(1, 1);
 
         // Animations
         anim = GetComponent<Animator>();
@@ -42,10 +39,7 @@ public class EnemyVariables : MonoBehaviour
         if(healthPoints <= 0 && isAlive)
         {
             Debug.Log("Enemy dies");
-            if (GetComponent<EnemyShooting>() != null)
-            {
-                anim.SetLayerWeight(1, 0);
-            }
+            anim.SetLayerWeight(1, 0);
             // Animations
             anim.SetTrigger("Death");
 
